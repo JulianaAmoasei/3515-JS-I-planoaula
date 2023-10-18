6715ebab307f2d85800433f6458432d23980a31d
JulianaAmoasei <late.parrot@gmail.com>
2023-10-18 17:31:24 -0300

    video: tipo Number add coisas

--- number.js
+++ number.js
@@ -1,11 +1,11 @@
 const notaPrimeiroBi = 8;
 const notaSegundoBi = 6.3;
 const notaTerceiroBi = -2;
-const notaQuartoBi = Number('5x');
+const notaQuartoBi = Number('5');
 console.log((notaQuartoBi));
 console.log(typeof NaN);
 
-const total = notaPrimeiroBi + notaSegundoBi + notaTerceiroBi + notaQuartoBi;
+const total = ((notaPrimeiroBi + notaSegundoBi + notaTerceiroBi + notaQuartoBi) / 4).toFixed(2);
 
 console.log(total);
 
