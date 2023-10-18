04e4e41c9a603fe2fa9f9bc8c393485bde1d773c
JulianaAmoasei <late.parrot@gmail.com>
2023-10-18 15:47:42 -0300

    videos 2.1 e 2.2

---
+++ script.js
@@ -0,0 +1,18 @@
+// explicar melhor variáveis
+// o que são variáveis
+// padrão de nomes do JS (os outros vão no PSM)
+// explicar a existência de var, let e const
+// deixar pra explicar no próximo vídeo
+
+// sistema de uma escola
+// estudantes, docentes, diretoria
+
+// ou var
+let estudante;
+
+if (1 > 0) {
+  estudante = 'Caroline';
+  console.log(estudante);
+}
+
+console.log(estudante);
