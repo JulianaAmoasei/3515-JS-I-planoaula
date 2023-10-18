269b758e20df8dfe4f66483d5e92004794b462a5
JulianaAmoasei <late.parrot@gmail.com>
2023-10-18 17:16:55 -0300

    video: tipo Number

---
+++ number.js
@@ -0,0 +1,14 @@
+const notaPrimeiroBi = 8;
+const notaSegundoBi = 6.3;
+const notaTerceiroBi = -2;
+const notaQuartoBi = Number('5x');
+console.log((notaQuartoBi));
+console.log(typeof NaN);
+
+const total = notaPrimeiroBi + notaSegundoBi + notaTerceiroBi + notaQuartoBi;
+
+console.log(total);
+
+const nome = 'Juliana';
+console.log('meu nome é ' + nome);
+
