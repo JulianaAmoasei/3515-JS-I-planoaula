bde5739c8c63f5ad580e7f2ffa3825ed8cd818fd
JulianaAmoasei <late.parrot@gmail.com>
2023-10-19 09:58:10 -0300

    video: boolean

---
+++ boolean.js
@@ -0,0 +1,16 @@
+const estudante = 'Fernando';
+const estaAprovado = true;
+
+if (estudante) {
+  console.log(`olá, ${estudante}`);
+} else {
+  console.log('quem é vc');
+}
+
+const nota = 0;
+
+if (nota) {
+  console.log(`sua nota é ${nota}`);
+} else {
+  console.log('você não tem notas lançadas');
+}
